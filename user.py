cf784f3ee2896d172f2e8f7833e579c387479863
Niel Conradie <nielconradie93@gmail.com>
2022-05-24 16:41:34 +0200

    Added user.py
    Added user.py file to number guess game.

---
+++ user.py
@@ -0,0 +1,33 @@
+import random
+
+
+def user_guess(number):
+    """ User guess the number. """
+    random_number = random.randint(1, number)
+
+    player_input = 0
+    while player_input != random_number:
+        # Requesting player input.
+        player_input = int(input(f"\nGuess the number between 1 and {number}: "))
+
+        # Too Low/High player input conditions.
+        if player_input < random_number:
+            print("\nToo Low! Guess again.")
+        elif player_input > random_number:
+            print("\nToo High! Guess again.")
+
+    print("\nCongratulations!")
+    print(f"You have guessed the number {random_number} correctly!")
+
+
+def play():
+    """ Guess the number. """
+    # Requesting player input.
+    player_input = int(input("Pick a range between 1 and "))
+
+    # Passing player input as argument.
+    user_guess(player_input)
+
+
+if __name__ == "__main__":
+    play()
