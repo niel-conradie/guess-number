7297ac1c53921d86660190cabc0a0f235532208b
Niel Conradie <nielconradie93@gmail.com>
2022-06-10 13:15:15 +0200

    Update run.py
    Added restart method to run.py file.

--- player-guess/run.py
+++ player-guess/run.py
@@ -5,17 +5,25 @@ def run():
     """ Player Guess. """
     run = PlayerGuess()
 
-    # Requesting user input.
-    user_input = run.user_input()
-    # Randomly select a number.
-    random_number = run.random_number(user_input)
-
-    user_input_guess = 0
-    while user_input_guess != random_number:
+    while True:
         # Requesting user input.
-        user_input_guess = run.user_input_guess(user_input)
-        # Validating user input conditions.
-        run.correct_guess(user_input_guess, random_number)
+        user_input = run.user_input()
+
+        # Randomly select a number.
+        random_number = run.random_number(user_input)
+
+        user_input_guess = 0
+        while user_input_guess != random_number:
+            # Requesting user input.
+            user_input_guess = run.user_input_guess(user_input)
+
+            # Validating user input conditions.
+            run.correct_guess(user_input_guess, random_number)
+
+        # Requesting user input.
+        run.restart()
+
+        continue
 
 
 if __name__ == "__main__":
