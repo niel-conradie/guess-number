e10b7935c534d9c109afaa0487d27571ba012ed4
Niel Conradie <nielconradie93@gmail.com>
2022-06-17 13:45:23 +0200

    Update run.py
    Added new methods to run.py file.

--- player-guess/run.py
+++ player-guess/run.py
@@ -8,7 +8,6 @@ def run():
     while True:
         # Requesting user input.
         user_input = run.user_input()
-
         # Randomly select a number.
         random_number = run.random_number(user_input)
 
@@ -16,10 +15,13 @@ def run():
         while user_input_guess != random_number:
             # Requesting user input.
             user_input_guess = run.user_input_guess(user_input)
-
             # Validating user input conditions.
             run.correct_guess(user_input_guess, random_number)
 
+        # Add point to the player score.
+        run.add_player_score()
+        # Display the scoreboard.
+        run.display_scoreboard()
         # Requesting user input.
         run.restart()
 
