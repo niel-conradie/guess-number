9c3cf812db49b0f6c70e4575183888ba62e9b776
Niel Conradie <nielconradie93@gmail.com>
2022-06-30 11:42:03 +0200

    Update computer_guess.py
    Fixed user_input methods conditional statements, and black code formatted file.

--- computer-guess/computer_guess.py
+++ computer-guess/computer_guess.py
@@ -2,37 +2,36 @@ import random
 
 
 class ComputerGuess:
-    """ A class to represent a guess game. """
+    """A class to represent a guess game."""
 
     def __init__(self):
-        """ Initialize class attributes. """
+        """Initialize class attributes."""
         self.computer_score = 0
 
     @staticmethod
     def user_input():
-        """ Requesting user input and validating number. """
+        """Requesting user input and validating number."""
         while True:
             try:
                 user_input = int(input("\nSelect a number to guess: "))
-                if user_input > 0:
-                    return user_input
-                elif user_input == 0:
+                if user_input == 0:
                     print("\nThat is not a valid number.")
-                else:
+                elif user_input < 0:
                     print("\nThat is a negative number.")
-                    continue
+                else:
+                    return user_input
             except ValueError:
                 print("\nThat is not a number.")
                 continue
 
     @staticmethod
     def computer_guess(number):
-        """ Computer guess the number. """
+        """Computer guess the number."""
         low = 1
         high = number
         guess = 0
-        user_input = ''
-        while user_input != 'c':
+        user_input = ""
+        while user_input != "c":
             if low != high:
                 guess = random.randint(low, high)
             else:
@@ -40,9 +39,12 @@ class ComputerGuess:
 
             # Requesting user input and validating choice.
             while True:
-                user_input = input(f"\nIf {guess} is correct then type 'C',\n"
-                                   f"too low 'L', and too high 'H': ").lower()
-                choices = ['c', 'l', 'h']
+                user_input = input(
+                    f"\nIf {guess} is correct then type 'C',\n"
+                    "too low 'L', and too high 'H': "
+                ).lower()
+
+                choices = ["c", "l", "h"]
                 if user_input in choices:
                     break
                 else:
@@ -50,34 +52,34 @@ class ComputerGuess:
                     continue
 
             # User input conditions.
-            if user_input == 'h':
+            if user_input == "h":
                 high = guess - 1
-            elif user_input == 'l':
+            elif user_input == "l":
                 low = guess + 1
 
         print(f"\nThe computer have guessed the number {guess} correctly!")
 
     def add_computer_score(self):
-        """ Add point to the computer score. """
+        """Add point to the computer score."""
         self.computer_score += 1
 
     def display_scoreboard(self):
-        """ Display the amount of correct answers. """
+        """Display the amount of correct answers."""
         print(f"Correct: {self.computer_score}")
 
     @staticmethod
     def restart():
-        """ Requesting user input and validating choice. """
+        """Requesting user input and validating choice."""
         while True:
             user_input = input("\nRestart? Yes/No: ").lower()
-            choices = ['yes', 'no']
+            choices = ["yes", "no"]
             if user_input not in choices:
                 print("\nPlease type 'yes' or 'no'")
                 continue
 
             # User input conditions.
-            if user_input == 'yes':
+            if user_input == "yes":
                 break
-            if user_input == 'no':
+            if user_input == "no":
                 print("\nThank you for playing!")
                 quit()
