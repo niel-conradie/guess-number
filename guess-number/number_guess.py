00695ef11f99b06c940cdc842284af2a98612049
Niel Conradie <nielconradie93@gmail.com>
2022-07-26 16:48:59 +0200

    Update number_guess.py
    Refactoring user_input() method.

--- guess-number/number_guess.py
+++ guess-number/number_guess.py
@@ -14,33 +14,25 @@ class NumberGuess:
     def user_input():
         """Requesting user input and validating choice."""
         while True:
+            print("\nSelect your game mode.")
+            print("\nComputer Guess: Type '1'")
+            print("Player Guess: Type '2'\n")
+
             try:
                 user_input = int(input("Enter: "))
             except ValueError:
-                print("\nThat is not a number.\n")
+                print("\nThat is not a number.")
                 continue
 
+            # User input validation conditions.
             choices = [1, 2]
             if user_input not in choices:
-                print(f"\n{user_input} is not an valid choice!\n")
+                print(f"\n{user_input} is not an valid choice!")
                 continue
-            else:
-                return user_input
-
-    @staticmethod
-    def user_input_allocation(user_input):
-        """Assign user input to appropriate game mode."""
-        if user_input == 1:
-            return ComputerGuess()
-        if user_input == 2:
-            return PlayerGuess()
-
-    @staticmethod
-    def display_options():
-        """Display game mode options."""
-        print("\nSelect your game mode.")
-        print("\nComputer Guess: Type '1'")
-        print("Player Guess: Type '2'\n")
+            elif user_input == 1:
+                return ComputerGuess()
+            elif user_input == 2:
+                return PlayerGuess()
 
     def add_computer_score(self):
         """Add point to the computer score."""
@@ -61,20 +53,16 @@ class NumberGuess:
     def start_game(self):
         """Starting number guess game."""
         while True:
-            # Display game mode options.
-            self.display_options()
             # Requesting user input.
             user_input = self.user_input()
-            # Assign player to appropriate game mode.
-            mode = self.user_input_allocation(user_input)
 
             while True:
                 # Starting the appropriate game mode.
-                game = mode.start_game()
+                game = user_input.start_game()
 
                 # Correct computer guess condition.
                 if game == "computer_guess":
-                    # # Add point to computer score.
+                    # Add point to computer score.
                     self.add_computer_score()
                     # Display computer correct guesses.
                     self.computer_scoreboard()
