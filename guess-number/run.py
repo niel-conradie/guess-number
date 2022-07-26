cb1537019bcc63fd005740534c22c2b06c872a79
Niel Conradie <nielconradie93@gmail.com>
2022-07-26 14:56:28 +0200

    Create run.py
    Create a launch file for ease of use.

---
+++ guess-number/run.py
@@ -0,0 +1,13 @@
+from number_guess import NumberGuess
+
+
+def run():
+    """Number Guess."""
+    run = NumberGuess()
+
+    # Starting the game.
+    run.start_game()
+
+
+if __name__ == "__main__":
+    run()
